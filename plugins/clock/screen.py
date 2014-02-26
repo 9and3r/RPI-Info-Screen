168c61fcc3a8faa4b9ec8d71e89e50b5a6ad9ca7
Ander <9and3r@gmail.com>
2014-02-26 18:44:21 +0100

    Clock resolution and design changes

--- plugins/clock/screen.py
+++ plugins/clock/screen.py
@@ -11,20 +11,14 @@ class myScreen(PiInfoScreen):
     
     def setPluginVariables(self):
         self.clockfont = os.path.join(self.plugindir, "resources", "SFDigitalReadout-Medium.ttf")         
-        self.myfont = pygame.font.Font(self.clockfont, 240)
-        self.myfontsmall = pygame.font.Font(self.clockfont, 120)
+        self.myfont = pygame.font.Font(self.clockfont, 100)
         
     def showScreen(self):
         self.surface.fill([0,0,0])
-        mytime = strftime("%H:%M")
-        mysecs = strftime("%S")
+        mytime = strftime("%H:%M:%S")
         clocklabel = self.myfont.render(mytime, 1, [255,255,255])
-        secondlabel = self.myfontsmall.render(mysecs, 1, [255,255,255])
         textpos = clocklabel.get_rect()
-        textpos.centerx = self.surface.get_rect().centerx
         textpos.centery = self.surface.get_rect().centery
-        secpos = [ textpos[0] + textpos[2] + 10, textpos[1] + 70 ]
-        self.surface.blit(secondlabel, secpos)
         self.surface.blit(clocklabel, textpos) 
 
         # Scale our surface to the required screensize before sending back
